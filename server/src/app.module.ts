124e2520fae7106cac33a35ef5e77b63539592da
mark42 <esh8876@naver.com>
2023-01-20 11:19:31 +0900

    0120-2
    0120-2

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,24 +3,29 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { PeopleModule } from "./people/people.module";
 import { BlahModule } from "./blah/blah.module";
-import { OnlineModule } from './online/online.module';
+import { OnlineModule } from "./online/online.module";
 // const MONGO_URI = process.env.MONGO_URI;
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      "mongodb+srv://study2022:1q2w3e4r@cluster0.ug0hmx4.mongodb.net/?retryWrites=true&w=majority",
-      { dbName: "project" },
-    ),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "env" ? ".dev.env" : ".test.env",
-      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      envFilePath: ".env",
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("MONGO_URI"),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "project",
+      }),
     }),
     AuthModule,
     PeopleModule,
